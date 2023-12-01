326d02c2aa279bc852fd7e4b2613f6fea1d4ecb9
Aidan Stambaugh <astambaugh2627@floridapoly.edu>
2023-12-01 11:54:12 -0500

    Adding Functionality/User Auth

--- Medicine/RxCheck/interactions/urls.py
+++ Medicine/RxCheck/interactions/urls.py
@@ -1,10 +1,9 @@
 
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('search_drug/', views.search_drug, name='search_drug'),
     path('search_results/', views.search_results, name='search_results'),
-    path('login/', views.login_page, name='login_page'),
     path('', views.index, name='index'),
 ]
